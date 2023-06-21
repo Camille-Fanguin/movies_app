36af14f1fdbdbb82b2e0965d459f1255b8780c7c
Apprenant17 <apprenant17@macbook-pro-de-apprenant17.home>
2023-06-21 16:47:10 +0200

    projet blank

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Apprenant83 on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
